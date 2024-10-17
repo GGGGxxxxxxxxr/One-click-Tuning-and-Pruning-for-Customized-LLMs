c3848821beddead6e8ead48a358e09b3eea04e5a
LEI LU <lu.lei1@northeastern.edu>
2024-10-16 23:44:56 -0400

    xxx

--- hypernet_llm.py
+++ hypernet_llm.py
@@ -96,7 +96,7 @@ class LLM_HyperStructure(nn.Module):
         self.Bi_GRU  = nn.GRU(64, 128, bidirectional=True)                  # [input dim, output_dim]
         self.h0      = torch.zeros(2, self.num_layers, 128)                 # [bidirect, LAYERS, output_dim]
 
-        self.inputs  = nn.Parameter(torch.Tensor(len(self.lw_structure), self.num_layers, 64))     # [sequence_len, LAYERS, input_dim]
+        self.inputs  = nn.Parameter(torch.Tensor(len(self.lw_structure), self.num_layers, 64)).to(dtype=torch.bfloat16)     # [sequence_len, LAYERS, input_dim]
         nn.init.orthogonal_(self.inputs)
         self.inputs.requires_grad=False                                     # 'input' itself is a untrainable nn.param
         
