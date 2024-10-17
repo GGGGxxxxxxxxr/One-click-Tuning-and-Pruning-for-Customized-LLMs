4de084af6b99d9a5cec6a4355756375937b46bda
LEI LU <lu.lei1@northeastern.edu>
2024-10-17 03:40:59 -0400

    xxx

--- alignment_function_llm.py
+++ alignment_function_llm.py
@@ -119,8 +119,8 @@ class Group_Lasso_regularization(nn.Module):
             # extract corrsponding LLM_DecoderLayer & Masks for this layer
             cur_layer = self.model.model.layers[layer_idx]              # CasualLM.model -> LMmodel.layer -> DecoderLayer
             # fsdp capatibility
-            with FSDP.summon_full_params(cur_layer, offload_to_cpu=True, rank0_only=True, writeback=False):
-                layer_wise_masks = [individual_mask[layer_idx,:].to("cpu") for individual_mask in pruning_masks]
+            with FSDP.summon_full_params(cur_layer):
+                layer_wise_masks = [individual_mask[layer_idx,:] for individual_mask in pruning_masks]
                 m_umlp = layer_wise_masks[-1].to("cpu")
                 m_out  = layer_wise_masks[-2]
                 m_K    = layer_wise_masks[:self.cfg.num_key_value_heads]
