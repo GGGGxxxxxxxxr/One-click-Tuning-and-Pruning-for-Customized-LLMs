76873a2f202a55f00ad1e0083a6ad2d387eae16a
LEI LU <lu.lei1@northeastern.edu>
2024-10-17 10:59:11 -0400

    xxx

--- alignment_function_llm.py
+++ alignment_function_llm.py
@@ -244,7 +244,7 @@ class Group_Lasso_regularization(nn.Module):
                         mlp_u_weight[m_umlp_out, :] /= w_norm.unsqueeze(1)
                         mlp_d_weight[:, m_umlp_out] /= w_norm.unsqueeze(0)
 
-                        tmp = (-self.lam * lr + w_norm).clamp(min=0) #* 0  # Apply mask scaling factor
+                        tmp = (-self.lam * lr + w_norm).clamp(min=0) * 0  # Apply mask scaling factor
 
                         mlp_g_weight[m_umlp_out, :] *= tmp.unsqueeze(1)
                         mlp_u_weight[m_umlp_out, :] *= tmp.unsqueeze(1)
@@ -276,7 +276,7 @@ class Group_Lasso_regularization(nn.Module):
                         mlp_u_weight[:, m_out] /= w_norm.unsqueeze(0)
                         attn_out_weight[m_out, :] /= w_norm.unsqueeze(1)
 
-                        tmp = (-self.lam * lr + w_norm).clamp(min=0) #* 0
+                        tmp = (-self.lam * lr + w_norm).clamp(min=0) * 0
 
                         mlp_g_weight[:, m_out] *= tmp.unsqueeze(0)
                         mlp_u_weight[:, m_out] *= tmp.unsqueeze(0)
@@ -305,7 +305,7 @@ class Group_Lasso_regularization(nn.Module):
                         attn_v_weight[V_mask, :] /= w_norm.unsqueeze(1)
                         attn_out_weight[:, V_mask_repeated] /= w_norm.unsqueeze(0)
 
-                        tmp = (-self.lam * lr + w_norm).clamp(min=0) #* 0
+                        tmp = (-self.lam * lr + w_norm).clamp(min=0) * 0
 
                         attn_v_weight[V_mask, :] *= tmp.unsqueeze(1)
                         attn_out_weight[:, V_mask_repeated] *= tmp.unsqueeze(0)
@@ -332,7 +332,7 @@ class Group_Lasso_regularization(nn.Module):
                         attn_k_weight[m_K_out, :] /= w_norm.unsqueeze(1)
                         attn_q_weight[m_Q_out, :] /= w_norm.unsqueeze(1)
 
-                        tmp = (-self.lam * lr + w_norm).clamp(min=0) #* 0
+                        tmp = (-self.lam * lr + w_norm).clamp(min=0) * 0
 
                         attn_k_weight[m_K_out, :] *= tmp.unsqueeze(1)
                         attn_q_weight[m_Q_out, :] *= tmp.unsqueeze(1)
