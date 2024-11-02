a875b6a59805dabe0f9fd43df5e1da676f1db35b
LEI LU <lu.lei1@northeastern.edu>
2024-11-02 01:01:14 -0400

    lora enabled

--- alignment_function_llm.py
+++ alignment_function_llm.py
@@ -215,9 +215,9 @@ class Group_Lasso_regularization(nn.Module):
             m_V    = layer_wise_masks[self.cfg.num_key_value_heads : 2 * self.cfg.num_key_value_heads]
 
             # process MLP_up_mask for LoRA weights
-            mlp_g_lora_B = cur_layer.mlp.gate_proj.lora_B.default.weight
-            mlp_u_lora_B = cur_layer.mlp.up_proj.lora_B.default.weight
-            mlp_d_lora_A = cur_layer.mlp.down_proj.lora_A.default.weight
+            mlp_g_lora_B = cur_layer.mlp.gate_proj.lora_B
+            mlp_u_lora_B = cur_layer.mlp.up_proj.lora_B
+            mlp_d_lora_A = cur_layer.mlp.down_proj.lora_A
 
             gl_loss      = ((1 - m_umlp).unsqueeze(1) * mlp_g_lora_B).pow(2).sum((1)).add(1e-8).pow(1/2.).sum() \
                                 + ((1 - m_umlp).unsqueeze(1) * mlp_u_lora_B).pow(2).sum((1)).add(1e-8).pow(1/2.).sum() \
@@ -225,9 +225,9 @@ class Group_Lasso_regularization(nn.Module):
             gl_list.append(gl_loss)
 
             # process attn_out_mask
-            attn_out_lora_B = cur_layer.self_attn.o_proj.lora_B.default.weight
-            mlp_g_lora_A    = cur_layer.mlp.gate_proj.lora_A.default.weight
-            mlp_u_lora_A    = cur_layer.mlp.up_proj.lora_A.default.weight
+            attn_out_lora_B = cur_layer.self_attn.o_proj.lora_B
+            mlp_g_lora_A    = cur_layer.mlp.gate_proj.lora_A
+            mlp_u_lora_A    = cur_layer.mlp.up_proj.lora_A
 
             gl_loss       = ((1 - m_out).unsqueeze(1) * attn_out_lora_B).pow(2).sum((1)).add(1e-8).pow(1/2.).sum() \
                           + ((1 - m_out).unsqueeze(0) * mlp_g_lora_A).pow(2).sum((0)).add(1e-8).pow(1/2.).sum()    \
@@ -239,8 +239,8 @@ class Group_Lasso_regularization(nn.Module):
             V_mask = torch.cat(m_V)
             V_mask_repeated = torch.cat([t.repeat(self.num_groups) for t in m_V])
             # b) compute gl for v_weight, v_bias, out_weight
-            attn_v_lora_B   = cur_layer.self_attn.v_proj.lora_B.default.weight
-            attn_out_lora_A = cur_layer.self_attn.o_proj.lora_A.default.weight
+            attn_v_lora_B   = cur_layer.self_attn.v_proj.lora_B
+            attn_out_lora_A = cur_layer.self_attn.o_proj.lora_A
 
             gl_loss       = ((1 - V_mask).unsqueeze(1) * attn_v_lora_B).pow(2).sum((1)).add(1e-8).pow(1/2.).sum() \
                           + ((1 - V_mask_repeated).unsqueeze(0) * attn_out_lora_A).pow(2).sum((0)).add(1e-8).pow(1/2.).sum()
@@ -250,8 +250,8 @@ class Group_Lasso_regularization(nn.Module):
             # a) concate K_split_masks into mask for original WK
             K_mask = torch.cat(m_K)
             Q_mask = torch.cat([t.repeat(self.num_groups) for t in m_K])
-            attn_k_lora_B = cur_layer.self_attn.k_proj.lora_B.default.weight
-            attn_q_lora_B = cur_layer.self_attn.q_proj.lora_B.default.weight
+            attn_k_lora_B = cur_layer.self_attn.k_proj.lora_B
+            attn_q_lora_B = cur_layer.self_attn.q_proj.lora_B
 
             gl_loss       = ((1 - K_mask).unsqueeze(1) * attn_k_lora_B).pow(2).sum((1)).add(1e-8).pow(1/2.).sum() \
                           + ((1 - Q_mask).unsqueeze(1) * attn_q_lora_B).pow(2).sum((1)).add(1e-8).pow(1/2.).sum() \
