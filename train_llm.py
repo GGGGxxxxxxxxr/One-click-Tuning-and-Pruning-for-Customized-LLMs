cc20bf548f74e597af73ed43d4f169ae4356b9dc
LEI LU <lu.lei1@northeastern.edu>
2024-11-03 23:40:11 -0500

    xxxx

--- train_llm.py
+++ train_llm.py
@@ -171,6 +171,7 @@ def target_llm_step(llm_model, input_ids, masks, attn_mask, epoch, args, gl_modu
         llm_loss = target_loss                         # in FSDP mode, we are forced to use GroupLasso DirectProjection to simulate such GL_loss backward effects
     else:
         llm_loss = gl_tensity * gl_loss #target_loss + gl_tensity * gl_loss
+        target_loss = target_loss.detach()
 
     scaler.scale(llm_loss).backward()
 
