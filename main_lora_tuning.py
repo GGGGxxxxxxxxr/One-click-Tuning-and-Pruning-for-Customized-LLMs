b749964c107900f707af37635458164a28a6b14a
LEI LU <lu.lei1@northeastern.edu>
2024-11-06 15:21:58 -0500

    xxx

--- main_lora_tuning.py
+++ main_lora_tuning.py
@@ -365,7 +365,8 @@ def main():
     print(tokenized_valsets[85])
     print("=====> NLP Dataset Initialization Done. <=====")
     # config training dataloader
-    data_collator  = DataCollatorWithPadding(tokenizer=tokenizer)
+    data_collator  = DataCollatorWithPadding(tokenizer=tokenizer, padding='longest')
+    
     ddp_sampler    = DistributedSampler(tokenized_datasets, num_replicas=world_size, rank=rank)
     ddp_sampler1   = DistributedSampler(tokenized_valsets, num_replicas=world_size, rank=rank)
     nlp_dataloader = DataLoader(
