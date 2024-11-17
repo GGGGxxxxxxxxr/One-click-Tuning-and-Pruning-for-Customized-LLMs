a340105dd4bfe52f0fe97ab708f3a21b2f1154a1
LEI LU <lu.lei1@northeastern.edu>
2024-11-16 22:09:13 -0500

    general dataset

--- main_llm_fsdp.py
+++ main_llm_fsdp.py
@@ -301,6 +301,7 @@ def main():
         print(f"pretraining_tp: {model_cfg.pretraining_tp}")
         tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token = api_token)
         tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        tokenizer.padding_side = 'left'
         model     = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", attn_implementation="sdpa", torch_dtype=torch.bfloat16, token = api_token).to(init_device)
         model.resize_token_embeddings(len(tokenizer))
         args.num_key_values = model_cfg.num_key_value_heads
