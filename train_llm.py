0ff31f814d5451ebd8eddfb9058cccd40c5f0bc2
LEI LU <lu.lei1@northeastern.edu>
2024-11-17 16:54:20 -0500

    bug fix

--- train_llm.py
+++ train_llm.py
@@ -189,7 +189,7 @@ def target_llm_step(llm_model, input_ids, labels, masks, attn_mask, epoch, args,
 
     if epoch >= args.start_epoch_regularization:
         if epoch == (args.epochs - 1):
-            gl_tensity = 10                               # force to set expected weights to ZERO
+            gl_tensity = 1000000                               # force to set expected weights to ZERO
             gl_module.grad_mul = gl_tensity
         else: 
             gl_tensity = 0.3
