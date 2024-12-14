e4dfb46929a3446b0c08b341c26c20bcb0cccfcc
LEI LU <lu.lei1@northeastern.edu>
2024-12-14 03:47:51 -0500

    xxx

--- train_llm.py
+++ train_llm.py
@@ -235,10 +235,10 @@ def target_llm_step(llm_model, input_ids, labels, masks, attn_mask, epoch, args,
 
     if epoch >= args.start_epoch_regularization:
         if epoch == (args.epochs - 1):
-            gl_tensity = 1000                              # force to set expected weights to ZERO
+            gl_tensity = 100                              # force to set expected weights to ZERO
             gl_module.grad_mul = gl_tensity
         else: 
-            gl_tensity = 0.3
+            gl_tensity = 0.03
             gl_module.grad_mul = gl_tensity
 
     if args.tuning_method != 'lora':
@@ -379,7 +379,7 @@ def hypernet_step(hypernet, llm_model, val_ids, labels, attn_mask, pruning_ratio
     alignment_loss          = (max_remaining_K_out_dim + max_remaining_V_out_dim) / args.num_key_values
     
     # e) sum the loss
-    hyper_loss = target_loss + 10 * ratio_loss #+ 0.00005 * alignment_loss
+    hyper_loss = target_loss + 5 * ratio_loss #+ 0.00005 * alignment_loss
 
     hyper_loss.backward()
 
