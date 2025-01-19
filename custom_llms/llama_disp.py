859a7487633dc47e21636b82304c6965234acb5e
LEI LU <lu.lei1@northeastern.edu>
2025-01-19 01:40:15 -0500

    version2.0: bf16 foward bug fix

--- custom_llms/llama_disp.py
+++ custom_llms/llama_disp.py
@@ -635,14 +635,16 @@ class LlamaSdpaAttention(LlamaAttention):
             dropout_p=self.attention_dropout if self.training else 0.0,
             is_causal=is_causal,
         )
-        print(attn_output.dtype)
+        
         attn_output = attn_output.transpose(1, 2).contiguous()
         attn_output = attn_output.view(bsz, q_len, -1)
-        print(attn_output.dtype)
+        
         # [ATP_DISP]: apply s2 to the attention_output projection      Alg.1.2
         if self.training != True:
             if m_s2 != None:
                 attn_output = self.o_proj(attn_output) * m_s2
+                print(m_s2.dtype)
+                print(attn_output.dtype)
             else:
                 attn_output = self.o_proj(attn_output)     
         else:
