c462a7eeed10ccd1d11982158e0ae74d4e5d4feb
LEI LU <lu.lei1@northeastern.edu>
2025-01-21 16:02:33 -0500

    version2.0: bug fixed in grouplasso_projection

--- train_llm_disp.py
+++ train_llm_disp.py
@@ -358,12 +358,12 @@ def llm_sp_train_one_epoch(nlp_dataloader, nlp_hypernet_dataloader, target_llm, 
             if torch.distributed.get_rank() == 0:
                 elapsed_time = time.time() - start_time
                 print(
-                    f"Time: {elapsed_time}s | "
+                    f"Time: {elapsed_time:.2f}s | "
                     f"Step: {i} | "
                     f"LLM Loss: {reduced_llm_loss:.3f} | "
-                    f"Hypernet Loss: {reduced_hyper_loss:.3f if reduced_hyper_loss is not None else 0.000} | "
-                    f"Valid Loss: {reduced_valid_loss:.3f if reduced_valid_loss is not None else 0.000} | "
-                    f"Ratio Loss: {reduced_ratio_loss:.3f if reduced_ratio_loss is not None else 0.000} | "
+                    f"Hypernet Loss: {reduced_hyper_loss:.3f} | "
+                    f"Valid Loss: {reduced_valid_loss:.3f} | "
+                    f"Ratio Loss: {reduced_ratio_loss:.3f} | "
                 )
 
                 start_time = time.time()
