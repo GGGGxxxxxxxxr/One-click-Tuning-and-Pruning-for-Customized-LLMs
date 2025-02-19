602109d7c5ee8efa3d7413cded51f596d001e8d4
LEI LU <lu.lei1@northeastern.edu>
2025-02-19 15:42:51 -0500

    version2: pruned model support

--- validate_pruning_checkpoint_pruned.py
+++ validate_pruning_checkpoint_pruned.py
@@ -47,7 +47,7 @@ def transform_output_layer_DISP(inputs, model_name=None):
 
 
 def initialize_model_and_tokenizer(pruned_ckpt_path=None, model_name=None):
-    ckpt_path = input_ckpt_path
+    ckpt_path = pruned_ckpt_path
     print(f"Loading pruned_model_checkpoint from {ckpt_path}.")
     checkpoint    = torch.load(ckpt_path, map_location=torch.device('cpu'))
     masks = checkpoint["pruning_mask"]
