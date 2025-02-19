9dbf94eb068ea40b69f297a1bf572c9eac507018
LEI LU <lu.lei1@northeastern.edu>
2025-02-19 13:57:20 -0500

    version2: pruned model support

--- conduct_model_compression.py
+++ conduct_model_compression.py
@@ -75,7 +75,7 @@ def compress_loralinear(layer, in_mask=None, out_mask=None):
     # **Compress original weight (linear.weight)**
     pruned_linear = nn.Linear(pruned_in_dim, pruned_out_dim, bias=False)
     print(pruned_linear.weight.shape)
-    pruned_weight = layer.linear.weight.data[select_out_idx, :][:, select_in_idx]
+    pruned_weight = layer.linear.weight.data[select_out_idx, :][:, select_in_idx].squeeze(2)
     print(pruned_weight.shape)
     pruned_linear.weight.data.copy_(pruned_weight)
 
