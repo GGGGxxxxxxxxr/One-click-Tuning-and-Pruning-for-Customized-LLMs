b31ca26626977da57fbb351b4d96337eb670e265
LEI LU <lu.lei1@northeastern.edu>
2025-03-12 20:01:00 -0400

    xxx

--- speed_test.py
+++ speed_test.py
@@ -4,11 +4,11 @@ from torch.utils.benchmark import Timer
 SparseSemiStructuredTensor._FORCE_CUTLASS = True
 
 # mask Linear weight to be 2:4 sparse
-mask = torch.Tensor([0, 0, 1, 1]).tile((4096, 1024)).cuda().bool()
-linear = torch.nn.Linear(4096, 4096).half().cuda().eval()
+mask = torch.Tensor([0, 0, 1, 1]).tile((4096, 1024)).cuda().bool().to(torch.bfloat16)
+linear = torch.nn.Linear(4096, 4096).bfloat16().cuda().eval()
 linear.weight = torch.nn.Parameter(mask * linear.weight)
 
-x = torch.rand(128, 4096).half().cuda()
+x = torch.rand(128, 4096).bfloat16().cuda()
 
 with torch.inference_mode():
     dense_output = linear(x)
