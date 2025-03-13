044befc11cbd3f57b3324472ef0ad97021a3ca0d
LEI LU <lu.lei1@northeastern.edu>
2025-03-12 20:25:28 -0400

    xxx

--- speed_test.py
+++ speed_test.py
@@ -1,18 +1,16 @@
 import torch
 from torch.sparse import to_sparse_semi_structured, SparseSemiStructuredTensor
 from torch.utils.benchmark import Timer
-SparseSemiStructuredTensor._FORCE_CUTLASS = False # Flase will use CUSPARSELt which is faster than CUTLASS
+SparseSemiStructuredTensor._FORCE_CUTLASS = True
 
 # mask Linear weight to be 2:4 sparse
-mask = torch.Tensor([0, 0, 1, 1]).tile((3072, 768)).cuda().bool().to(torch.bfloat16)
-linear = torch.nn.Linear(3072, 3072).bfloat16().cuda().eval()
+mask = torch.Tensor([0, 0, 1, 1]).tile((3072, 2560)).cuda().bool()
+linear = torch.nn.Linear(10240, 3072).half().cuda().eval()
 linear.weight = torch.nn.Parameter(mask * linear.weight)
 
-x = torch.rand(1, 3072).bfloat16().cuda()
+x = torch.rand(3072, 10240).half().cuda()
 
 with torch.inference_mode():
-    
-
     dense_output = linear(x)
     dense_t = Timer(stmt="linear(x)",
                     globals={"linear": linear,
@@ -21,12 +19,11 @@ with torch.inference_mode():
     # accelerate via SparseSemiStructuredTensor
     linear.weight = torch.nn.Parameter(to_sparse_semi_structured(linear.weight))
 
-
     sparse_output = linear(x)
     sparse_t = Timer(stmt="linear(x)",
                     globals={"linear": linear,
                              "x": x}).blocked_autorange().median * 1e3
 
     # sparse and dense matmul are numerically equivalent
-    assert torch.allclose(sparse_output, dense_output, atol=1e-2)
+    assert torch.allclose(sparse_output, dense_output, atol=1e-3)
     print(f"Dense: {dense_t:.3f}ms Sparse: {sparse_t:.3f}ms | Speedup: {(dense_t / sparse_t):.3f}x")
